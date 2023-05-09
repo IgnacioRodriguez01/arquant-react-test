47ff261c07d2d1212ce60ed1527500f7265425b6
IgnacioRodriguez01 <ignacioandresrodriguez01@gmail.com>
2023-05-09 13:54:39 -0300

    Minor fixes en items

--- arquant-react-test/src/components/Item.jsx
+++ arquant-react-test/src/components/Item.jsx
@@ -5,12 +5,12 @@ const Item = ({ item, fn }) => {
 
     return <li className="list-group-item bg-white d-flex justify-content-between align-items-center mb-2 border rounded">
         <div>
-            <p className="fs-4 mb-1"><span className="fw-bold">Name:</span> {name}</p>
-            <p className="mb-0"><span className="fw-bold">Height:</span> {height} cm</p>
-            <p className="mb-2"><span className="fw-bold">Mass:</span> {mass} kg</p>
+            <p className="fs-4 mb-1"><span className="fw-bold">Nombre:</span> {name}</p>
+            <p className="mb-0"><span className="fw-bold">Altura:</span> {height} cm</p>
+            <p className="mb-2"><span className="fw-bold">Peso:</span> {mass} kg</p>
         </div>
         
-        <button onClick={() => fn(item)} type="button" className="btn btn-danger h-50 fw-bold">Borrar</button>
+        <button onClick={() => fn(item)} type="button" className="btn btn-danger fw-bold" style={{height:"2.5rem"}}>Borrar</button>
     </li>
 };
 
